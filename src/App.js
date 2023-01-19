e076bc73aa23d08847da34e4e700143e98a71264
daniel simonson <dansimonson@mariposaweb.net>
2023-01-18 21:05:09 -0600

    starting form

--- src/App.js
+++ src/App.js
@@ -1,45 +1,103 @@
 import React, { useState, useEffect } from "react";
-import employee from "./employee";
-import employees from "./employees.json";
+import { nanoid } from "nanoid";
+import data from "./mock-data.json";
 import "./App.css";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [title, setTitle] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  useEffect(() => {
-    if (employees) {
-      setData(employees.employees);
-    }
-  }, []);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    let uniqueId = Math.random().toFixed(2).toString() * 1000;
-    let newItem = {
-      id: uniqueId,
-      title: title,
-      firstName: firstName,
-      lastName: lastName,
-    };
-    setData([...data, newItem])
-    setTitle("");
-    setFirstName("");
-    setLastName("");
-
- 
+  console.log('data: ', data)
+  const [team, setTeam] = useState(data);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+  })
+  const handleFormChange = (event) => {
+    event.preventDefault();
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+    const newFormData = { ...formData };
+    newFormData[fieldName] = fieldValue;
+    setFormData(newFormData)
   }
-  const editMe = (e, id) => {
-    e.preventDefault();
-    console.log("e", e);
-    console.log("id: ", id);
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newMember = {
+      id: nanoid(),
+      fullName: formData.fullName,
+      address: formData.address,
+      phoneNumber: formData.phoneNumber,
+      email: formData.email,
+    };
+    const newTeamMember = [...team, newMember];
+    setTeam(newTeamMember);
   };
-  const deleteMe = (e) => {};
   return (
     <>
-      <div className="mainSection">
+      <div className="app-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {team.map((teamMember) => (
+              <tr>
+                <td>{teamMember.fullName}</td>
+                <td>{teamMember.address}</td>
+                <td>{teamMember.phoneNumber}</td>
+                <td>{teamMember.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h2>Add A Contact</h2>
+
+        <form onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            required="required"
+            placeholder="Enter a name..."
+            name="fullName"
+            onChange={handleFormChange}
+          ></input>
+          <input
+            type="text"
+            required="required"
+            placeholder="Enter an address..."
+            name="address"
+            onChange={handleFormChange}
+          ></input>
+          <input
+             type="text"
+             required="required"
+             placeholder="Enter a phone number..."
+             name="phoneNumber"
+             onChange={handleFormChange}
+          ></input>
+          <input
+            type="email"
+            required="required"
+            placeholder="Enter an email..."
+            name="email"
+            onChange={handleFormChange}
+          ></input>
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
+/**
+ * <div className="mainSection">
         <h1 className="centerDiv">Simple Form</h1>
         <form onSubmit={handleSubmit} className="centerForm">
           <input
@@ -62,9 +120,9 @@ function App() {
           />
           <input type="submit" className="inputSection" />
         </form>
-        {data.length > 0 && (
+        {employees.length > 0 && (
           <>
-            {data.map((d) => (
+            {/* {employees.map((d) => (
               <div className="displaySection" key={d.id}>
                 <p>title: {d.title} </p>
                 <p>firstName: {d.firstName} </p>
@@ -72,12 +130,7 @@ function App() {
                 <button onClick={(e) => editMe(e, d.id)}>Edit</button>
                 <button>Delete</button>
               </div>
-            ))}
-          </>
-        )}
-      </div>
-    </>
-  );
-}
-
-export default App;
+            ))} */
+//</>
+// )}
+//</div>
